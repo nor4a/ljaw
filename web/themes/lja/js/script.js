0820fc13b9930a517203994b6de70e0f49cdb54e
Mateusz Woźniak <mateusz@oveno.pl>
2016-05-11 14:07:46 +0200

    Icons and logo as SVG files, added favicon

--- web/themes/lja/js/script.js
+++ web/themes/lja/js/script.js
@@ -134,6 +134,29 @@
             minimumHeight();
         });
 
+        // Replace SVG image into inline
+
+        $('img.svg').each(function(){
+            var $img = $(this);
+            var imgID = $img.attr('id');
+            var imgClass = $img.attr('class');
+            var imgURL = $img.attr('src');
+            $.get(imgURL, function(data) {
+                var $svg = $(data).find('svg');
+                if(typeof imgID !== 'undefined') {
+                    $svg = $svg.attr('id', imgID);
+                }
+                if(typeof imgClass !== 'undefined') {
+                    $svg = $svg.attr('class', imgClass+' replaced-svg');
+                }
+                $svg = $svg.removeAttr('xmlns:a');
+                if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                    $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+                }
+                $img.replaceWith($svg);
+            }, 'xml');
+        });
+
     });
 
 })(jQuery);

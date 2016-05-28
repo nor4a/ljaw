70c51dc90005b392a623c5097b4b02e542d2288a
root <root@d8dev.idynamic.eu>
2016-05-28 19:32:48 +0300

    module files

--- web/modules/ljaw/js/ljaw.js
+++ web/modules/ljaw/js/ljaw.js
@@ -3,7 +3,7 @@
   Drupal.behaviors.ljaw = {
     attach: function (context, settings) {
       $('#node-contact-edit-form').each(function(){
-        $('#edit-title-0-value').prop('READONLY',true).css('background-color','#ddd');
+        $('#edit-title-0-value').prop('readonly',true).css('background-color','#ddd');
         $('#edit-field-post-0-value').change(setContactsTitle);
         $('#edit-field-department').change(setContactsTitle);
       });
